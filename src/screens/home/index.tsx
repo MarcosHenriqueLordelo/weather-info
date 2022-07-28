302ce9523e25d2779c50f5e374dcd0af5bb1350c
Marcos Marques <marcosmarquesdev@gmail.com>
2022-07-28 00:20:09 -0300

    create home screen layout

--- src/screens/home/index.tsx
+++ src/screens/home/index.tsx
@@ -1,11 +1,84 @@
 import React from "react";
+import { Image, View } from "react-native";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
+import MaterialIcon from "@expo/vector-icons/MaterialIcons";
 
-import { Container, HelloWorld } from "./styles";
+import {
+  Author,
+  BuiltBy,
+  BuiltByContainer,
+  Container,
+  Content,
+  FeeledTemp,
+  Header,
+  HeaderTitleSubtitle,
+  InfoContainer,
+  Quote,
+  QuoteContainer,
+  Subtitle,
+  Temperature,
+  Title,
+  WeatherCondion,
+} from "./styles";
+
+import { TouchableOpacity } from "react-native-gesture-handler";
+import useMain from "../../contexts/main/useMain";
+import Loading from "../../components/Loading";
+import getBackGroundImage from "../../utils/getBackGroundImage";
+import moment from "moment";
+import useUi from "../../contexts/ui/useUi";
 
 const Index: React.FC = () => {
+  const insets = useSafeAreaInsets();
+  const { loading, strings } = useUi();
+  const { weather, getWeatherInfo, quote } = useMain();
+
+  if (!weather || loading || !quote) return <Loading />;
+
   return (
     <Container>
-      <HelloWorld>Hello World!</HelloWorld>
+      <Image
+        source={getBackGroundImage(weather.id, weather.day)}
+        style={{ position: "absolute", width: "100%", height: "100%" }}
+        resizeMethod="resize"
+      />
+      <View
+        style={{
+          height: insets.top,
+          width: "100%",
+        }}
+      />
+      <Content>
+        <Header>
+          <HeaderTitleSubtitle>
+            <Title>{moment().format("ll")}</Title>
+            <Subtitle>{`${weather.city}, ${weather.country}`}</Subtitle>
+          </HeaderTitleSubtitle>
+          <TouchableOpacity onPress={getWeatherInfo}>
+            <MaterialIcon size={32} name="refresh" color={"#FFFFFF"} />
+          </TouchableOpacity>
+        </Header>
+
+        <InfoContainer>
+          <Temperature>{weather.temp}°</Temperature>
+          <WeatherCondion>{weather.description}</WeatherCondion>
+          <FeeledTemp>{`${strings.feelsLike} ${weather.feelsLike}°`}</FeeledTemp>
+        </InfoContainer>
+
+        <QuoteContainer>
+          <Quote>{`"${quote.text}"`}</Quote>
+          <Author>{`- ${quote.author}`}</Author>
+        </QuoteContainer>
+      </Content>
+      <BuiltByContainer>
+        <BuiltBy>Built By Marcos Marques</BuiltBy>
+      </BuiltByContainer>
+      <View
+        style={{
+          height: insets.bottom,
+          width: "100%",
+        }}
+      />
     </Container>
   );
 };
--- src/screens/home/styles.ts
+++ src/screens/home/styles.ts
@@ -2,9 +2,86 @@ import styled from "styled-components/native";
 
 export const Container = styled.View`
   flex: 1;
-  justify-content: center;
-  align-items: center;
-  background-color: aliceblue;
 `;
 
-export const HelloWorld = styled.Text``;
+export const Content = styled.View`
+  flex: 1;
+  padding: 16px 24px;
+`;
+
+export const Header = styled.View`
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+export const Title = styled.Text`
+  color: #ffffff;
+  font-size: 22px;
+  font-family: "EduRegular";
+`;
+
+export const Subtitle = styled.Text`
+  color: #ffffff;
+  text-transform: uppercase;
+  font-family: "EduBold";
+  font-size: 18px;
+  letter-spacing: 2px;
+`;
+
+export const HeaderTitleSubtitle = styled.View``;
+
+export const InfoContainer = styled.View`
+  margin-top: 70px;
+`;
+
+export const Temperature = styled.Text`
+  color: #ffffff;
+  font-family: "EduBold";
+  font-size: 80px;
+`;
+
+export const WeatherCondion = styled.Text`
+  color: #ffffff;
+  font-size: 36px;
+  font-family: "EduSemiBold";
+  text-transform: capitalize;
+`;
+
+export const FeeledTemp = styled.Text`
+  color: #ffffff;
+  font-size: 22px;
+  font-family: "EduRegular";
+`;
+
+export const QuoteContainer = styled.View`
+  margin-top: 100px;
+`;
+
+export const Quote = styled.Text`
+  color: #ffffff;
+  font-family: "EduRegular";
+  font-size: 22px;
+  text-align: center;
+`;
+
+export const Author = styled.Text`
+  color: #ffffff;
+  font-family: "EduMedium";
+  font-size: 18px;
+  text-align: center;
+  margin-top: 8px;
+`;
+
+export const BuiltByContainer = styled.TouchableOpacity`
+  align-self: center;
+  background-color: ${({ theme }) => theme.colors.section};
+  border-radius: 12px;
+  padding: 6px 12px;
+  margin-bottom: 16px;
+`;
+
+export const BuiltBy = styled.Text`
+  font-family: "EduMedium";
+  color: #ffffff;
+`;
